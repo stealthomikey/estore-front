c143dbe8ef6a813263601e8c738dbfaf430d2c75
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2025-05-22 22:34:20 +0100

    removed cart link

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 
 const CartIcon = () => {
   return (
-    <Link href='/cart' className='group relative'>
-        <ShoppingBag className='w-5 h-5 hover:text-shop_light_green hoverEffect'/>
-        <span className='absolute -top-1 -right-1 bg-shop-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>0</span>
-    </Link>   
+
+        <>
+          <ShoppingBag className='w-5 h-5 hover:text-shop_light_green hoverEffect'/>
+          <span className='absolute -top-1 -right-1 bg-shop-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>0</span>
+        </>
+
   )
 }
 
