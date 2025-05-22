ce7669686ea79dce7bd8a4dd9d209df816c40484
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2025-05-22 22:43:36 +0100

    fixed positioning

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 
 const CartIcon = () => {
   return (
-
-        <>
-          <ShoppingBag className='w-5 h-5 hover:text-shop_light_green hoverEffect'/>
-          <span className='absolute -top-1 -right-1 bg-shop-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>0</span>
-        </>
-
+    <div className='group relative'>
+        <ShoppingBag className='w-5 h-5 hover:text-shop_light_green hoverEffect'/>
+        <span className='absolute -top-1 -right-1 bg-shop-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>0</span>
+    </div>   
   )
 }
 
